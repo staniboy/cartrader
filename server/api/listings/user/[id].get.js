877eaf82d209567d8a047687950008a9caddd890
staniboy <stanley.v@outlook.com>
2023-06-03 16:28:30 +0400

    feat: get user listings endpoint

---
+++ server/api/listings/user/[id].get.js
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default defineEventHandler(async (event) => {
+  const { id: userId } = event.context.params;
+
+  return prisma.listing.findMany({
+    where: {
+      userId: userId,
+    },
+    select: {
+      image: true,
+      id: true,
+      model: true,
+      make: true,
+      price: true,
+    },
+  });
+});
