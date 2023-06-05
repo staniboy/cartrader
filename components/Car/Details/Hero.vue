06827237537c3822b24601254289a4edee541668
staniboy <stanley.v@outlook.com>
2023-06-05 15:59:43 +0400

    fix: heading not displaying

--- components/Car/Details/Hero.vue
+++ components/Car/Details/Hero.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mt-10">
-    <NuxtImg class="w-full" :src="car.url" />
-    <h1 class="mt-10 text-4xl">{{ car.name }}</h1>
+    <NuxtImg class="w-full" :src="car.image" />
+    <h1 class="mt-10 text-4xl">{{ car.make }} {{ car.model }}</h1>
     <div class="text-slate-500 flex text-lg mt-3 border-b pb-5 justify-between">
       <div class="flex">
         <p class="mr-2">{{ car.seats }} seats</p>
