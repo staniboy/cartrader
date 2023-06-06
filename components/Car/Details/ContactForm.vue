8f99e6fa45770b5d39db23a9950abf86c985e1f0
staniboy <stanley.v@outlook.com>
2023-06-06 16:22:41 +0400

    refactor: working vee form

--- components/Car/Details/ContactForm.vue
+++ components/Car/Details/ContactForm.vue
@@ -1,15 +1,100 @@
 <template>
-  <div class="mt-10">
-    <div class="flex w-[800px] justify-between">
-      <input type="text" class="border p-1" placeholder="Name" />
-      <input type="text" class="border p-1" placeholder="Email" />
-      <input type="text" class="border p-1" placeholder="Phone" />
-    </div>
-    <div class="flex w-[800px] mt-4">
-      <textarea class="border p-1 w-full" placeholder="Message"></textarea>
-    </div>
-    <button class="bg-blue-400 text-white px-10 py-3 rounded mt-4">
-      Submit
-    </button>
+  <div class="flex flex-col gap-2 w-[800px] mt-10">
+    <span
+      v-if="statusMessage"
+      class="text-md bg-sky-500 text-white font-bold rounded-md p-4"
+      >{{ statusMessage }}</span
+    >
+    <Form @submit="handleSubmit">
+      <div class="flex w-[800px] justify-between">
+        <!-- Name -->
+        <div class="flex flex-col gap-2">
+          <Field
+            as="input"
+            type="text"
+            name="name"
+            class="border p-1"
+            placeholder="Name"
+            :rules="validation.fields.name"
+          />
+          <ErrorMessage
+            as="span"
+            name="name"
+            class="text-red-700 text-sm font-bold"
+          />
+        </div>
+        <!-- Email -->
+        <div class="flex flex-col gap-2">
+          <Field
+            as="input"
+            type="text"
+            name="email"
+            class="border p-1"
+            placeholder="Email"
+            :rules="validation.fields.email"
+          />
+          <ErrorMessage
+            as="span"
+            name="email"
+            class="text-red-700 text-sm font-bold"
+          />
+        </div>
+
+        <!-- Phone -->
+        <div class="flex flex-col gap-2">
+          <Field
+            as="input"
+            type="number"
+            name="phone"
+            class="border p-1"
+            placeholder="Phone"
+            :rules="validation.fields.phone"
+          />
+          <ErrorMessage
+            as="span"
+            name="phone"
+            class="text-red-700 text-sm font-bold"
+          />
+        </div>
+      </div>
+      <!-- Message -->
+      <div class="flex flex-col gap-2 w-[800px] mt-4">
+        <Field
+          as="textarea"
+          name="message"
+          class="border p-1 w-full"
+          placeholder="Type your message here"
+          :rules="validation.fields.message"
+        />
+        <ErrorMessage
+          as="span"
+          name="message"
+          class="text-red-700 text-sm font-bold"
+        />
+      </div>
+      <button class="bg-blue-400 text-white px-10 py-3 rounded mt-4">
+        Submit
+      </button>
+    </Form>
   </div>
 </template>
+<script setup>
+const { message: validation } = useValidation();
+const statusMessage = ref();
+const route = useRoute();
+const handleSubmit = async (values, { resetForm }) => {
+  await $fetch(`/api/car/listings/${route.params.id}/message`, {
+    method: "post",
+    body: {
+      ...values,
+    },
+  })
+    .catch((err) => {
+      //TODO: Implement error catch
+    })
+    .then(() => {
+      resetForm();
+      statusMessage.value = "Message Sent!";
+    });
+};
+</script>
