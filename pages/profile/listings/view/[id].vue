fc7b07bacb1f6ed8ee68d96b2ff2f5eab0779884
staniboy <stanley.v@outlook.com>
2023-06-06 16:29:58 +0400

    feat: retriving data from the server

--- pages/profile/listings/view/[id].vue
+++ pages/profile/listings/view/[id].vue
@@ -1,18 +1,19 @@
 <template>
-  <GenericCard>
-    <template v-slot:title>John Smith</template>
-    <template v-slot:content
-      >Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet omnis
-      culpa soluta ducimus, repellat quod maxime quis cum rem consequuntur
-      repudiandae, saepe mollitia velit numquam nostrum voluptatibus suscipit
-      quam provident.
-    </template>
+  <GenericCard v-for="message in messages">
+    <template v-slot:title>{{ message.name }}</template>
+    <template v-slot:content> {{ message.message }} </template>
     <template v-slot:footer>
       <div class="flex flex-row gap-4">
-        <p class="text-green-700 text-sm cursor-pointer">johnsmith@gmail.com</p>
-        <p class="text-blue-700 text-sm cursor-pointer">555-555-5555</p>
+        <!-- TODO: Make links interactive -->
+        <p class="text-green-700 text-sm cursor-pointer">{{ message.email }}</p>
+        <p class="text-blue-700 text-sm cursor-pointer">{{ message.phone }}</p>
       </div>
     </template>
   </GenericCard>
 </template>
-<script setup></script>
+<script setup>
+const route = useRoute();
+const { data: messages } = useFetch(
+  `/api/car/listings/${route.params.id}/message`
+);
+</script>
