64ec9fa1cefd37b419944822bc04ce1edfbb47f1
staniboy <stanley.v@outlook.com>
2023-06-12 15:36:48 +0400

    feat: interactive links

--- pages/profile/listings/view/[id].vue
+++ pages/profile/listings/view/[id].vue
@@ -1,13 +1,20 @@
 <template>
-  <!-- TODO: Display "there is no new messages if there is no messages" -->
+  <!-- TODO: Display "there is no messages if there is no messages" -->
   <GenericCard v-for="message in messages">
     <template v-slot:title>{{ message.name }}</template>
     <template v-slot:content> {{ message.message }} </template>
     <template v-slot:footer>
       <div class="flex flex-row gap-4">
-        <!-- TODO: Make links interactive -->
-        <p class="text-green-700 text-sm cursor-pointer">{{ message.email }}</p>
-        <p class="text-blue-700 text-sm cursor-pointer">{{ message.phone }}</p>
+        <a
+          :href="`mailto: ${message.email}`"
+          class="text-green-700 text-sm cursor-pointer"
+          >{{ message.email }}</a
+        >
+        <a
+          :href="`tel:${message.phone}`"
+          class="text-blue-700 text-sm cursor-pointer"
+          >{{ message.phone }}</a
+        >
       </div>
     </template>
   </GenericCard>
