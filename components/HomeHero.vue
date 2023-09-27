ce59d59d97532dfc1c893180812dffd72ef98808
staniboy <stanley.v@outlook.com>
2023-09-27 16:42:35 +0400

    fix: navbar offset

--- components/HomeHero.vue
+++ components/HomeHero.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="relative h-screen w-100 bg-no-repeat bg-cover bg-bottom bg-[url(https://unsplash.com/photos/bkyIc6uJIdU/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MzB8fGx1eHVyeSUyMGNhcnxlbnwwfHx8fDE2ODIyODI4MjM&force=true&w=1920)]"
+    class="relative -mt-[69px] h-screen w-100 bg-no-repeat bg-cover bg-bottom bg-[url(https://unsplash.com/photos/bkyIc6uJIdU/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MzB8fGx1eHVyeSUyMGNhcnxlbnwwfHx8fDE2ODIyODI4MjM&force=true&w=1920)]"
   >
     <div class="absolute z-10 w-full h-full flex justify-center items-center">
       <div class="text-center m-5 w-full md:w-2/3 xl:w-2/5">
