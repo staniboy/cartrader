13abd952531a28f466990d7227403d22058d7a33
staniboy <stanley.v@outlook.com>
2023-09-30 19:24:39 +0400

    refactor: rework based on card

--- components/Car/Card.vue
+++ components/Car/Card.vue
@@ -1,30 +1,30 @@
 <template>
-  <GenericCard>
-    <template v-slot:image>
-      <NuxtImg
-        :src="`${config.public.supabase.url}/storage/v1/object/public/images/${car.image}`"
-        class="w-full h-full object-cover"
-        @click="navigateTo(`/car/${car.make} ${car.model}/${car.id}`)"
-      />
-    </template>
-    <template v-slot:title>
-      <span
-        class="cursor-pointer text-blue-700"
-        @click="navigateTo(`/car/${car.make} ${car.model}/${car.id}`)"
-      >
-        {{ car.make }} {{ car.model }}
-      </span>
-    </template>
-    <template v-slot:content>
-      <span class="line-clamp-3">{{ car.description }}</span>
-    </template>
-    <template v-slot:append>
-      <slot></slot>
-    </template>
-    <template v-slot:footer>
-      <h1 class="mt-auto text-xl">${{ car.price }}</h1>
-    </template>
-  </GenericCard>
+  <Card class="md:h-[200px]">
+    <div class="flex flex-col md:flex-row h-full">
+      <!-- Image -->
+      <div class="min-w-[300px]">
+        <NuxtImg
+          class="w-full h-full object-cover"
+          :src="`${config.public.supabase.url}/storage/v1/object/public/images/${car.image}`"
+          @click="navigateTo(`/car/${car.make} ${car.model}/${car.id}`)"
+        />
+      </div>
+      <!-- Content -->
+      <div class="p-4 flex flex-col gap-2 justify-between">
+        <div class="flex flex-row justify-between">
+          <div
+            class="cursor-pointer text-blue-700 text-2xl"
+            @click="navigateTo(`/car/${car.make} ${car.model}/${car.id}`)"
+          >
+            {{ car.make }} {{ car.model }}
+          </div>
+          <slot></slot>
+        </div>
+        <div class="line-clamp-3">{{ car.description }}</div>
+        <div class="text-xl">${{ car.price }}</div>
+      </div>
+    </div>
+  </Card>
 </template>
 <script setup>
 const config = useRuntimeConfig();
