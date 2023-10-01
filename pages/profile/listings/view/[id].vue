5e7fd26e5610f78685a61c55a42aa2c8ecda02d0
staniboy <stanley.v@outlook.com>
2023-10-01 17:44:31 +0400

    refactor: rework of messages

--- pages/profile/listings/view/[id].vue
+++ pages/profile/listings/view/[id].vue
@@ -1,23 +1,22 @@
 <template>
   <!-- TODO: Display "there is no messages if there is no messages" -->
-  <GenericCard v-for="message in messages">
-    <template v-slot:title>{{ message.name }}</template>
-    <template v-slot:content> {{ message.message }} </template>
-    <template v-slot:footer>
-      <div class="flex flex-row gap-4">
-        <a
-          :href="`mailto: ${message.email}`"
-          class="text-green-700 text-sm cursor-pointer"
-          >{{ message.email }}</a
-        >
-        <a
-          :href="`tel:${message.phone}`"
-          class="text-blue-700 text-sm cursor-pointer"
-          >{{ message.phone }}</a
-        >
-      </div>
-    </template>
-  </GenericCard>
+  <Card v-for="message in messages" class="p-4 flex flex-col gap-4">
+    <div class="text-xl">{{ message.name }}</div>
+    <div>{{ message.message }}</div>
+
+    <div class="flex flex-row gap-4">
+      <a
+        :href="`mailto: ${message.email}`"
+        class="text-green-700 text-sm cursor-pointer"
+        >{{ message.email }}</a
+      >
+      <a
+        :href="`tel:${message.phone}`"
+        class="text-blue-700 text-sm cursor-pointer"
+        >{{ message.phone }}</a
+      >
+    </div>
+  </Card>
 </template>
 <script setup>
 const route = useRoute();
