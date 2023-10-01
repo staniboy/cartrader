5e996f2f345829ce958281a84ee35db857b05857
staniboy <stanley.v@outlook.com>
2023-10-01 13:10:48 +0400

    fix: add grow to content

--- components/Car/Card.vue
+++ components/Car/Card.vue
@@ -2,7 +2,7 @@
   <Card class="md:h-[200px]">
     <div class="flex flex-col md:flex-row h-full">
       <!-- Image -->
-      <div class="min-w-[300px]">
+      <div class="w-[300px]">
         <NuxtImg
           class="w-full h-full object-cover"
           :src="`${config.public.supabase.url}/storage/v1/object/public/images/${car.image}`"
@@ -10,7 +10,8 @@
         />
       </div>
       <!-- Content -->
-      <div class="p-4 flex flex-col gap-2 justify-between">
+      <div class="p-4 flex flex-col gap-2 justify-between grow">
+        <!-- Header -->
         <div class="flex flex-row justify-between">
           <div
             class="cursor-pointer text-blue-700 text-2xl"
