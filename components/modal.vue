dc09ecd98e4c06f83b1ae33a08c3acfce4c62881
staniboy <stanley.v@outlook.com>
2023-10-02 16:46:16 +0400

    feat: modal

---
+++ components/modal.vue
@@ -0,0 +1,16 @@
+<template>
+  <dialog open class="bg-inherit">
+    <card class="p-4"><slot></slot></card>
+  </dialog>
+</template>
+<script setup>
+const props = defineProps({
+  title: String,
+  content: String,
+});
+</script>
+<style lang="css">
+dialog::backdrop {
+  background-color: black;
+}
+</style>
