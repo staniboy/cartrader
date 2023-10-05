422511bc15e3417001494353d5256d665f87c9f1
staniboy <stanley.v@outlook.com>
2023-10-05 17:40:35 +0400

    feat: add autofocus, rename to app-button

---
+++ components/AppButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button
+    :autofocus="props.autofocus"
+    class="py-1 px-4 bg-sky-500 hover:bg-sky-600 rounded items-center text-center text-white cursor-pointer"
+  >
+    <slot></slot>
+  </button>
+</template>
+<script setup>
+const props = defineProps({
+  autofocus: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+});
+</script>
--- components/Button.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div
-    class="py-1 px-4 bg-sky-500 hover:bg-sky-600 rounded items-center text-center text-white cursor-pointer"
-  >
-    <slot></slot>
-  </div>
-</template>
