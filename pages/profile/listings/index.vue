58ee92416376663cafb877aa2af5286f2b1ec6ac
staniboy <stanley.v@outlook.com>
2023-10-05 17:41:05 +0400

    feat: implement confirmation modal

--- pages/profile/listings/index.vue
+++ pages/profile/listings/index.vue
@@ -1,12 +1,20 @@
 <template>
-  <modal class="backdrop:bg-black" ref="deleteModal"
-    >Are you sure you want to delete this listing?
+  <!-- Modal -->
+  <modal class="backdrop:bg-black" ref="confirmModal">
+    <div class="flex flex-col gap-4">
+      <div class="text-md">Are you sure you want to delete this listing?</div>
+      <div class="flex gap-2 justify-end">
+        <app-button autofocus @click="hideModal">Cancel</app-button>
+        <app-button @click="deleteListing(listingId)">Delete</app-button>
+      </div>
+    </div>
   </modal>
+  <!-- Modal End -->
   <div class="flex items-center gap-4 justify-between py-4">
     <div class="text-4xl">My Listings</div>
-    <Button>
+    <app-button>
       <nuxt-link to="/profile/listings/create">Add</nuxt-link>
-    </Button>
+    </app-button>
   </div>
   <div class="flex flex-col gap-2">
     <ClientOnly>
@@ -38,7 +46,7 @@
               </nuxt-link>
               <p
                 class="text-red-400 cursor-pointer"
-                @click="deleteListing(listing.id)"
+                @click="showModal(listing.id)"
               >
                 Delete
               </p>
@@ -51,15 +59,22 @@
 </template>
 <script setup>
 // TODO: Add delete confirmation
-const deleteModal = ref();
+const confirmModal = ref();
+const listingId = ref();
 const user = useSupabaseUser();
 const config = useRuntimeConfig();
 const { data: listings } = await useFetch(
   `/api/car/listings/user/${user.value.id}`
 );
+const hideModal = () => confirmModal.value.hide();
+const showModal = (id) => {
+  listingId.value = id;
+  confirmModal.value.show();
+};
+
 const deleteListing = async (id) => {
-  deleteModal.value.show();
-  // await $fetch(`/api/car/listings/${id}`, { method: "DELETE" });
-  // listings.value = listings.value.filter((listing) => listing.id !== id);
+  await $fetch(`/api/car/listings/${id}`, { method: "DELETE" });
+  listings.value = listings.value.filter((listing) => listing.id !== id);
+  confirmModal.value.hide();
 };
 </script>
